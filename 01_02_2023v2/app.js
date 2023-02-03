0444dbc3ec3c34dfdd420ff796ab4b5a84fb38a0
Mainak Dev <mainak@mainak.local>
2023-02-03 11:07:05 +0530

    Feb 3 final commit

--- 01_02_2023v2/app.js
+++ 01_02_2023v2/app.js
@@ -2,8 +2,23 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res) => {
-    console.log('Inside middleware');
+
+app.use('/',(req, res, next) => {
+    console.log('Home page middleware');
+    next();
 });
+//middleware
+app.use('/products',(req, res) => {
+    res.send('<h1>Products page</h1>');
+});
+//middleware
+app.use('/account',(req, res) => {
+    res.send('<h1>Accounts page</h1>');
+});
+
+app.use('/',(req, res) => {
+  
+});
+
 
 app.listen(3000);
