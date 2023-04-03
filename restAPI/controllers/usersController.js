fcd2d55591cd7903e3e49dec481641a3e920d55d
Mainak Dev <mainakonandroid@gmail.com>
2023-04-03 09:28:10 +0530

    added get & post routes

--- restAPI/controllers/usersController.js
+++ restAPI/controllers/usersController.js
@@ -1,12 +1,24 @@
 const user = require('../model/userModel')
+const asyncHandler = require('express-async-handler')
 
-getUsers = (req, res, next) => {
-    
-    res.json({message: 'GET route'})
-}
+getUsers =  asyncHandler(async(req, res, next) => {
+    const allUsers = await user.find();
+    res.status(200).json(allUsers)
+})
 
-createUser = (req, res, next) => {
-    res.json({message: 'POST route'})
+createUser = async(req, res, next) => {
+    if(!req.body.username){
+        res.status(400)
+        throw new Error('Username required')
+    } else if(!req.body.password){
+        res.status(400)
+        throw new Error('Password required')
+    }
+    const newUser = await user.create({
+        username: req.body.username,
+        password: req.body.password
+    })
+    res.json(newUser)
 }
 
 updateUser = (req, res, next) => {
--- restAPI/index.js
+++ restAPI/index.js
@@ -9,6 +9,9 @@ const port = process.env.PORT || 5000
 
 const app = express()
 
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
 app.use('/api/users', require('./routes/userRoutes'))
 
 app.listen(port, () => {
