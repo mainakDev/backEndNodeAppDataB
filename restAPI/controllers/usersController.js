bf5d6ff9d9f0144aadd7331bdc4842f9a06836ca
Mainak Dev <mainakonandroid@gmail.com>
2023-04-05 09:02:55 +0530

    Final Commit REST API

--- restAPI/controllers/usersController.js
+++ restAPI/controllers/usersController.js
@@ -1,12 +1,12 @@
-const user = require('../model/userModel')
+const User = require('../model/userModel')
 const asyncHandler = require('express-async-handler')
 
 getUsers =  asyncHandler(async(req, res, next) => {
-    const allUsers = await user.find();
+    const allUsers = await User.find();
     res.status(200).json(allUsers)
 })
 
-createUser = async(req, res, next) => {
+createUser = asyncHandler(async(req, res, next) => {
     if(!req.body.username){
         res.status(400)
         throw new Error('Username required')
@@ -14,20 +14,45 @@ createUser = async(req, res, next) => {
         res.status(400)
         throw new Error('Password required')
     }
-    const newUser = await user.create({
+    const newUser = await User.create({
         username: req.body.username,
         password: req.body.password
     })
     res.json(newUser)
-}
+})
 
-updateUser = (req, res, next) => {
-    res.json({message: 'PUT route'})
-}
+updateUser = asyncHandler(async(req, res, next) => {
+    const user = await User.findById(req.params.id)
+    if(!user){
+        res.status(400)
+        throw new Error(`User does't exists`)
+    }
+    const updatedUser = await User.findByIdAndUpdate(req.params.id, req.body, {new: true})
+    res.json(updatedUser)
+})
 
-deleteUser = (req, res, next) => {
-    res.json({message: 'DELETE route'})
-}
+// const updateUser = asyncHandler(async (req, res, next) => {
+//     const user = await User.findById(req.params.id)
+
+//     if(!user){
+//         res.status(400)
+//         throw new Error('User not Found')
+//     }
+
+//     const updatedUser = await User.findByIdAndUpdate(req.params.id, req.body, {new: true,})
+//     res.status(200).json(updatedUser);
+// })
+
+
+deleteUser = asyncHandler(async(req, res, next) => {
+    const user = await User.findById(req.params.id)
+    if(!user){
+        res.status(400)
+        throw new error(`User doesn't exists`)
+    }
+    await user.deleteOne()
+    res.json({message: `User ${req.params.id} deleted`})
+})
 
 module.exports = {
     getUsers,
--- restAPI/index.js
+++ restAPI/index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const dotenv = require('dotenv').config()
 const mongoose = require('mongoose')
 const connectDB = require('./config/db')
+const cors = require('cors')
+const {errorHandler} = require('./middleware/errorMiddleware')
 
 connectDB()
 
@@ -9,10 +11,13 @@ const port = process.env.PORT || 5000
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
 app.use('/api/users', require('./routes/userRoutes'))
+
+app.use(errorHandler)
 
 app.listen(port, () => {
     console.log(`Server started at port: ${port}`);
